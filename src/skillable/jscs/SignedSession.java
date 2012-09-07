d57bd75b0980d462028f0cebb323c615723bb312
Philipp Gayret <philipp@skillable.eu>
2012-09-07 20:52:52 +0200

    Exception "handling"

--- src/skillable/jscs/SignedSession.java
+++ src/skillable/jscs/SignedSession.java
@@ -2,10 +2,8 @@ package skillable.jscs;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.security.GeneralSecurityException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
@@ -17,7 +15,6 @@ import java.util.HashMap;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.bind.DatatypeConverter;
 
 public class SignedSession extends HashMap<String, Object> {
 
@@ -57,36 +54,43 @@ public class SignedSession extends HashMap<String, Object> {
 
 	// -- Serialization and deserialization ---------------
 
-	public Cookie toCookie() throws GeneralSecurityException, IOException {
-		SignedObject signedObject = new SignedObject(this, Private, PrivateSig);
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		ObjectOutputStream oos = new ObjectOutputStream(baos);
-		oos.writeObject(signedObject);
-		oos.close();
-		return new Cookie(CookieName, DatatypeConverter.printBase64Binary(baos
-				.toByteArray()));
-	}
-
-	public static SignedSession fromCookie(Cookie cookie) throws IOException,
-			ClassNotFoundException, GeneralSecurityException {
-		byte[] data = DatatypeConverter.parseBase64Binary(cookie.getValue());
-		ByteArrayInputStream bais = new ByteArrayInputStream(data);
-		ObjectInputStream ois = new ObjectInputStream(bais);
-		SignedObject signedObject = (SignedObject) ois.readObject();
-		if (signedObject.verify(Public, PublicSig)) {
-			return (SignedSession) signedObject.getObject();
+	public Cookie toCookie() {
+		try {
+			SignedObject signedObject = new SignedObject(this, Private,
+					PrivateSig);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ObjectOutputStream oos = new ObjectOutputStream(baos);
+			oos.writeObject(signedObject);
+			oos.close();
+			return new Cookie(CookieName, new String(baos.toByteArray()));
+		} catch (Exception e) {
+			// GeneralSecurityException & IOException: Both should never happen
+			return null;
 		}
-		return null;
 	}
 
-	public void save(HttpServletResponse resp) throws IOException,
-			GeneralSecurityException {
+	public static SignedSession fromCookie(Cookie cookie) {
+		try {
+			byte[] data = cookie.getValue().getBytes();
+			ByteArrayInputStream bais = new ByteArrayInputStream(data);
+			ObjectInputStream ois = new ObjectInputStream(bais);
+			SignedObject signedObject = (SignedObject) ois.readObject();
+			if (signedObject.verify(Public, PublicSig)) {
+				return (SignedSession) signedObject.getObject();
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			// GeneralSecurityException & IOException: Both should never happen
+			return null;
+		}
+	}
+
+	public void save(HttpServletResponse resp) {
 		resp.addCookie(this.toCookie());
 	}
 
-	public static SignedSession load(HttpServletRequest req)
-			throws IOException, ClassNotFoundException,
-			GeneralSecurityException {
+	public static SignedSession load(HttpServletRequest req) {
 		for (Cookie cookie : req.getCookies()) {
 			if (CookieName.equals(cookie.getName())) {
 				return SignedSession.fromCookie(cookie);
